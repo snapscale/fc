4b2a911aa58e3692539f862ddae225bcc30d0f65
Matt Witherspoon <32485495+spoonincode@users.noreply.github.com>
2020-02-18 21:24:29 -0500

    don't use GMP

--- secp256k1/libsecp256k1-config.h
+++ secp256k1/libsecp256k1-config.h
@@ -6,10 +6,9 @@
 #define HAVE___INT128 1
 
 //use GMP for bignum
-#define HAVE_LIBGMP 1
-#define USE_NUM_GMP 1
-#define USE_FIELD_INV_NUM 1
-#define USE_SCALAR_INV_NUM 1
+#define USE_NUM_NONE 1
+#define USE_FIELD_INV_BUILTIN 1
+#define USE_SCALAR_INV_BUILTIN 1
 
 //use impls best for 64-bit
 #define USE_FIELD_5X52 1
